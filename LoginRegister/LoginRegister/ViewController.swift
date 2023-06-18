8dc50c5bf041911458e1deea0be6936973e3aeb6
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-18 17:18:26 +0600

    design basic UI structure

---
+++ LoginRegister/LoginRegister/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  LoginRegister
+//
+//  Created by BTSL.SAJID on 18/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var segmentedPicker: UISegmentedControl!
+    @IBOutlet weak var loginSegmentView: UIView!
+    @IBOutlet weak var registerSegmentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.bringSubviewToFront(loginSegmentView)
+    }
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.view.bringSubviewToFront(loginSegmentView)
+        case 1:
+            self.view.bringSubviewToFront(registerSegmentView)
+        default:
+            break
+        }
+    }
+    
+
+}
+
